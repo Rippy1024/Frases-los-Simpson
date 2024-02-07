7eede1b175233f89c1a0e0dcf8e72c09d249461d
Nano <luxorius07@gmail.com>
2024-02-07 09:29:56 -0300

    se agrego el readme

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,39 @@
-import './App.css'
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import logo from "./assets/logosimpson.png";
-import { Button, Container,} from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
 import Frase from "./components/Frase";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function App() {
+  const [personaje, setPersonaje] = useState({});
+
+  useEffect(() => {
+    consultarAPI();
+  }, []);
+
+  const consultarAPI = async () => {
+    //consultar una peticion get a la api;
+    const respuesta = await fetch(
+      "https://thesimpsonsquoteapi.glitch.me/quotes"
+    );
+    const datos = await respuesta.json();
+    console.log(datos[0]);
+    setPersonaje(datos[0]);
+  }
 
   return (
     <>
-  <Container className="text-center my-5">
+      <Container className="text-center my-5">
         <img src={logo} alt="Logo de los simpson" className="w-50" />
-        <Frase></Frase>
-        <Button variant="warning" >
+        <Frase personaje={personaje}></Frase>
+        <Button variant="danger" onClick={consultarAPI}>
           Obtener frase
         </Button>
       </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
